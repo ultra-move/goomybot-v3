2f73c2976faf3dcc880c5632d78d9aaeca96e3cf
ultramove <198922539+ultra-move@users.noreply.github.com>
2025-07-06 20:17:19 -0500

    fix logic

--- main.py
+++ main.py
@@ -1787,7 +1787,7 @@ async def professor_monitor_task(interval_seconds: int):
 
 async def start_challenge():
     active_challenge = await storage_manager.get_active_professor(1)
-    if active_challenge and active_challenge.start_time >= datetime.now(timezone.utc) - timedelta(minutes=15):
+    if active_challenge and active_challenge.start_time <= datetime.now(timezone.utc) - timedelta(minutes=15):
         await reset_challenge(None)
         await create_challenge()
     if not active_challenge:
